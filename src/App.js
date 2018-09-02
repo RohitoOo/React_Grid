b549e150e6857559ea2240fba96d81f409adaa4b
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-09-02 19:59:11 +0530

    Breakdown of React Grid Library

--- src/App.js
+++ src/App.js
@@ -21,73 +21,25 @@ defaultColumnValues} from './data/generator'
 
 const getChildRows = (row, rootRows) => (row ? row.items : rootRows);
 
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      columns: [
-        { name: 'name', title: 'Name' },
-        { name: 'city', title: 'City' },
-      ],
-      tableColumnExtensions: [
-        { columnName: 'name', width: 300 },
-      ],
-      data: generateRows({
-        columnValues: {
-          ...defaultColumnValues,
-          items: ({ random }) => (random() > 0.5
-            ? generateRows({
-              columnValues: {
-                ...defaultColumnValues,
-                items: () => (random() > 0.5
-                  ? generateRows({
-                    columnValues: {
-                      ...defaultColumnValues,
-                    },
-                    length: Math.trunc(random() * 5) + 1,
-                    random,
-                  })
-                  : null),
-              },
-              length: Math.trunc(random() * 3) + 1,
-              random,
-            })
-            : null),
-        },
-        length: 2,
-      }),
-    };
-  }
 
   render() {
-    const {
-      data, columns, tableColumnExtensions, expandedRowIds, loading,
-    } = this.state;
     return (
       <div className="App">
-       <Paper>
         <Grid
-          rows={data}
-          columns={columns}
-        >
-          <SelectionState />
-          <TreeDataState />
-          <CustomTreeData
-            getChildRows={getChildRows}
-          />
-          <IntegratedSelection />
-          <Table
-            columnExtensions={tableColumnExtensions}
-          />
-          <TableHeaderRow />
-          {/* TableTreeColumn - renders the table column with an indent, toggle button, and selection controls */}
-          <TableTreeColumn
-            for="name"
-          />
+                rows={[
+                  { location: 0, employeeName: 'DevExtreme',  },
+                  { location: 1, employeeName: 'DevExtreme Reactive' },
+                ]}
+                columns={[
+                  { name: 'location', title: 'Location' },
+                  { name: 'employeeName', title: 'EmployeeName' },
+                 
+                ]}>
+                <Table />
+
+                <TableHeaderRow />
         </Grid>
-      </Paper>
       </div>
     );
   }
